e7858562e33afc68673131cc8d499993db8c57f0
Petre Popescu <pepopesc@microsoft.com>
2018-03-22 11:29:41 +0200

    Third commit

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var Express = require('express')
 var app = Express()
 
 app.use((req,res)=>{
-    console.log('Hello from console')
+    console.log('Hello from console 2')
     res.send(req.headers)
 })
 
